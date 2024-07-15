11ca192e181325636fadb68ef503df0b775959f0
HarshPatel8279 <hhenu8279@gmail.com>
2024-07-16 00:05:20 +0530

    added hello_world using c and c++ library

---
+++ hello_world.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+using namespace std;
+int main(){
+    puts("Hello world");
+    return 0;
+}
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     cout<<"Hello world";
+//     return 0;
+// }
